6e6f68919c58dca25dad2ac5d6037b7ad3dbfa93
GrayQuarter <john@grayquarter.com>
2025-02-04 10:16:09 -0800

    created by Gray Quarter Git Push Tool

---
+++ Scripts/ASB;PUBLICWORKS!~!~!~.js
@@ -0,0 +1,13 @@
+//ASB:PublicWorks/*/*/*
+
+//var parcelArr = new Array;
+//loadParcelAttributes(parcelArr, capId);
+
+////Script Load Validated Parcel Attributes
+//include("SMC_ASB_LoadValidatedParcelAttr");
+
+////Script Message FEMA
+//include("SMC_ASB_MsgFema");
+
+////Script Message City
+//include("SMC_ASB_MsgCity");
