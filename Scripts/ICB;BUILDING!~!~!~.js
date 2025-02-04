e9b4c3514834a70f415bcc85b389ff59e1330e33
GrayQuarter <john@grayquarter.com>
2025-02-04 10:20:06 -0800

    created by Gray Quarter Git Push Tool

---
+++ Scripts/ICB;BUILDING!~!~!~.js
@@ -0,0 +1,9 @@
+//ICB:Building/~/~/~
+
+//Script 237 Inspection Cancelled/Reschedule Block
+include("BLD_237_ISB_InspectionCancelRescheduleBlock");
+
+//logDebug("Show this messasge");
+
+//var getSchedDate = aa.env.getValue("ScheduledDate");
+//logDebug(getSchedDate);
