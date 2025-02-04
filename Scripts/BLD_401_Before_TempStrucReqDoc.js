726051bf1b490d073dc8a5b931e252d9bbf75425
GrayQuarter <john@grayquarter.com>
2025-02-04 10:18:52 -0800

    created by Gray Quarter Git Push Tool

---
+++ Scripts/BLD_401_Before_TempStrucReqDoc.js
@@ -0,0 +1,105 @@
+/*******************************************************
+| Script Title: tempStrucReqDoc(ID401)
+| Created by: Jei Yang
+| Created on: 19Apr22
+| Event: Before
+| Usage: If no doc of type "Plan Set | PLANS" Or if ASI
+| "Will traffic / pedestrian control be required for this event?" is "Yes" and no document of type
+| "Traffic / Pedestrian Control Plan | TRAFPED" Then block submittal, display message
+| "The following document(s) are missing. To continue please upload a document of the following type(s):"
+| + "-" + docType + "For further details please refer to the instructional text below."
+| Modified by: ()
+*********************************************************/
+/*------------------------------------------------------------------------------------------------------/
+| Main Loop
+/-----------------------------------------------------------------------------------------------------*/
+try {
+    (function () {
+        Avo_LogDebug("ID401 tempStrucReqDoc()", 1);
+
+        var allReqDocTypes = new Object();
+        allReqDocTypes["Plan Set | PLANS"] = true;
+
+        var trafficPedControl = String(AInfo[
+            "Will traffic / pedestrian control be required for this event?"]);
+        Avo_LogDebug("Will traffic / pedestrian control Req?(" + trafficPedControl + ")", 2); //debug
+
+        if (trafficPedControl.toUpperCase() == "YES") {
+            allReqDocTypes["Traffic / Pedestrian Control Plan | TRAFPED"] = true;
+        }
+        // Check docs
+        var allDocs = new Array();
+        var result = aa.document.getDocumentListByEntity(capId, "TMP_CAP");
+        if (result.getSuccess() != true) {
+            Avo_LogDebug("Failed to find any documents. "
+                + result.errorType + ': ' + result.errorMessage, 1);
+        } else {
+            allDocs = result.getOutput().toArray();
+        }
+
+        Avo_LogDebug("Total Docs(" + allDocs.length + ")", 2);  //debug
+
+        for (var i in allDocs) {
+            var docModel = allDocs[i];
+
+            var docId = docModel.documentNo;
+            Avo_LogDebug("Doc ID(" + docId + ")", 2);	//debug
+
+            var filePath = docModel.fileName;
+            Avo_LogDebug("Path(" + filePath + ")", 2);	//debug
+
+            var name = docModel.docName;
+            Avo_LogDebug("Name(" + name + ")", 2);  //debug
+
+            var group = docModel.docGroup;
+            Avo_LogDebug("Group(" + group + ")", 2); //debug
+
+            var category = docModel.docCategory;
+            Avo_LogDebug("Category(" + category + ")", 2); //debug
+
+            if (!(category in allReqDocTypes)) {
+                continue;
+            }
+
+            delete allReqDocTypes[category];
+            Avo_LogDebug("Found " + category + " doc", 1);
+        }
+
+        if (Object.keys(allReqDocTypes).length == 0) {
+            return;
+        }
+
+        Avo_LogDebug(br + "No " + Object.keys(allReqDocTypes).join(", ") + " doc(s) found", 1);
+
+        cancel = true;
+        showDebug = false;
+        showMessage = true;
+
+        var msg = "The following document(s) are missing. \
+To continue please upload a document of the following type(s):";
+        for (var i in Object.keys(allReqDocTypes)) {
+            msg += "\n- " + Object.keys(allReqDocTypes)[i];
+        }
+
+        msg += "\nFor further details please refer to the instructional text below.";
+
+        comment(msg);
+    })();
+
+    aa.env.setValue("CapModel", cap);
+}
+catch (ex) {
+    Avo_LogDebug("ERROR: " + ex.message, 1);
+}
+
+/*------------------------------------------------------------------------------------------------------/
+| END Main Loop
+/------------------------------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------------------------------/
+| BEGIN Functions
+/------------------------------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------------------------------/
+| END Functions
+/------------------------------------------------------------------------------------------------------*/
