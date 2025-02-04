a2f14865a4062b59f9c2f5604c79d86691d9cee7
GrayQuarter <john@grayquarter.com>
2025-02-04 10:16:40 -0800

    created by Gray Quarter Git Push Tool

---
+++ Scripts/BB_FOLDERS.js
@@ -0,0 +1,37 @@
+[
+    {
+        "Id": 181319046,
+        "Type": "",
+        "Alias": "",
+        "Module": "BUILDING",
+        "Directory": "BUILDING"
+    },
+    {
+        "Id": 181319375,
+        "Type": "",
+        "Alias": "",
+        "Module": "PLANNING",
+        "Directory": "PLANNING"
+    },
+    {
+        "Id": 181319426,
+        "Type": "",
+        "Alias": "",
+        "Module": "PUBLICWORKS",
+        "Directory": "PUBLIC WORKS"
+    },
+    {
+        "Id": 181319479,
+        "Type": "",
+        "Alias": "",
+        "Module": "REALPROPERTY",
+        "Directory": "REAL PROPERTY"
+    },
+    {
+        "Id": 181319527,
+        "Type": "",
+        "Alias": "",
+        "Module": "WASTEMGMT",
+        "Directory": "WASTE MANAGEMENT"
+    }
+]
