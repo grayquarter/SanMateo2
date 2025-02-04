72a44c7567efe24b2e0ae6a5856edbe87048d74e
GrayQuarter <john@grayquarter.com>
2025-02-04 10:16:08 -0800

    created by Gray Quarter Git Push Tool

---
+++ Scripts/ASA;PUBLICWORKS!~!~!~.js
@@ -0,0 +1,17 @@
+//ASA:PublicWorks/*/*/*
+
+//var parcelArr = new Array;
+//loadParcelAttributes(parcelArr, capId);
+
+////Script Set Community Area and Sewer District
+//include("PW_ASA_ASIUA_SetAreaAndDistrict");
+
+//copyParcelGisObjects();
+
+////Script Set Inspection Area
+//include("SMC_ASA_SetInspArea");
+
+//formatAddressAndOwnerZips(capId);
+
+// BlueBeam integration - keep at the bottom
+include("BB_ASA");
