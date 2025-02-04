d2a48503b5314fab4f1793939edc85cccc9d2740
GrayQuarter <john@grayquarter.com>
2025-02-04 10:20:43 -0800

    created by Gray Quarter Git Push Tool

---
+++ Scripts/SMC_ASB_MsgFema.js
@@ -0,0 +1,11 @@
+/*******************************************************
+| Script/Function: msgFema()
+| Created by: Nicolaj Bunting
+| Created on: 18Aug20
+| Usage: on submit display message with "FEMA" parcel attribute
+| Modified by: ()
+*********************************************************/
+(function () {
+    showMessage = true;
+    comment("FEMA = " + parcelArr["ParcelAttribute.FEMA"]);  // test to see if a parcel attribute is visible;
+})();
