346682140ab4a120a82636f0f1579543a2b577e6
GrayQuarter <john@grayquarter.com>
2025-02-04 10:17:25 -0800

    created by Gray Quarter Git Push Tool

---
+++ Scripts/BLD_109_WTUA_IssueOtcPermit.js
@@ -0,0 +1,32 @@
+/*******************************************************
+| Script/Function: issueOtcPermit() - (ID109)
+| Created by: Nicolaj Bunting
+| Created on: 2Dec20
+| Usage: When task "Planning Review" has status set to "Passed" Or "Passed with Conditions" close task "Review Consolidation" with 
+| status of "Approved", close task "Ready to Issue Permit" with status of "Permit Issued", call ID54
+| Modified by: ()
+*********************************************************/
+(function () {
+    var taskName = "Review Consolidation";
+    var status = "Approved";
+    var success = closeTask(taskName, status, "", "");
+    if (success === false) {
+        Avo_LogDebug('Failed to close task "' + taskName + '" with status of "' + status + '"', 1);
+        return;
+    }
+
+    Avo_LogDebug('Closed task "' + taskName + '" with status of "' + status + '"', 1);
+
+    taskName = "Ready to Issue Permit";
+    status = "Permit Issued";
+    success = branchTask(taskName, status, "", "");
+    if (success === false) {
+        Avo_LogDebug('Failed to close task "' + taskName + '" with status of "' + status + '"', 1);
+        return;
+    }
+
+    Avo_LogDebug('Closed task "' + taskName + '" with status of "' + status + '"', 1);
+
+    //Script 54 Send Permit Issued Email
+    include("BLD_054_ASA_CTRCA_SendPermitIssuedEmail");
+})();
