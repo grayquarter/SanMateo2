bb3358a4d644c5dd686f4974ec8950baae6cc4a2
GrayQuarter <john@grayquarter.com>
2025-02-04 10:17:44 -0800

    created by Gray Quarter Git Push Tool

---
+++ Scripts/BLD_212_Before_PV_ESTO_RequireDoc.js
@@ -0,0 +1,110 @@
+/*******************************************************
+| Script Title: pvESTORequireDoc(ID212)
+| Created by: Jei Yang
+| Created on: 20Jan21
+| Event: Before
+| Usage: If no document of type "PV, ESS, EVC Plans | PVS" Or ASI
+| "Is the exisiting electrical service 200 amps or greater?" is "No" and no doc of type
+| "Load Calculations | Load" Then block submittal, display message
+| "The following document(s) are missing. To continue please upload a document of the following type(s):"
+| + "-" + docType + "For further details please refer to the instructional text below."
+| Modified by: ()
+*********************************************************/
+/*------------------------------------------------------------------------------------------------------/
+| Main Loop
+/-----------------------------------------------------------------------------------------------------*/
+try {
+    (function () {
+        Avo_LogDebug("ID212 pvESTORequireDoc()", 1);
+        var allReqDocTypes = new Object();
+
+        allReqDocTypes["PV, ESS, EVC Plans | PVS"] = true;
+
+        var TwohundredAmpsOrGreater = String(AInfo[
+            "Is the existing electrical service 200 amps or greater?"]);
+        Avo_LogDebug("200 amps or greater?(" + TwohundredAmpsOrGreater + ")", 2);   //debug
+
+        if (TwohundredAmpsOrGreater.toUpperCase() == "NO") {
+            allReqDocTypes["Load Calculations | LOAD"] = true;
+        }
+
+        // Check docs
+        var allDocs = new Array();
+        var result = aa.document.getDocumentListByEntity(capId, "TMP_CAP");
+        if (result.getSuccess() != true) {
+            Avo_LogDebug("Failed to find any documents. "
+                + result.errorType + ': ' + result.errorMessage, 1);
+        } else {
+            allDocs = result.getOutput().toArray();
+        }
+
+        Avo_LogDebug("Total Docs(" + allDocs.length + ")", 2);  //debug
+
+        for (var i in allDocs) {
+            var docModel = allDocs[i];
+
+            var docId = docModel.documentNo;
+            Avo_LogDebug("Doc ID(" + docId + ")", 2);	//debug
+
+            var filePath = docModel.fileName;
+            Avo_LogDebug("Path(" + filePath + ")", 2);	//debug
+
+            var name = docModel.docName;
+            Avo_LogDebug("Name(" + name + ")", 2);  //debug
+
+            var group = docModel.docGroup;
+            Avo_LogDebug("Group(" + group + ")", 2); //debug
+
+            var category = docModel.docCategory;
+            Avo_LogDebug("Category(" + category + ")", 2); //debug
+
+            if (!(category in allReqDocTypes)) {
+                continue;
+            }
+
+            //var uploadDate = new Date(docModel.fileUpLoadDate.time);
+            //Avo_LogDebug("Upload Date(" + aa.util.formatDate(uploadDate, "MM/dd/yyyy")
+            //+ ")", 2);   //debug
+
+            delete allReqDocTypes[category];
+            Avo_LogDebug("Found " + category + " doc", 1);
+        }
+
+        if (Object.keys(allReqDocTypes).length == 0) {
+            return;
+        }
+
+        Avo_LogDebug(br + "No " + Object.keys(allReqDocTypes).join(", ") + " doc(s) found", 1);
+
+        cancel = true;
+        showDebug = false;
+        showMessage = true;
+
+        var msg = "The following document(s) are missing. \
+To continue please upload a document of the following type(s):";
+        for (var i in Object.keys(allReqDocTypes)) {
+            msg += "\n- " + Object.keys(allReqDocTypes)[i];
+        }
+
+        msg += "\nFor further details please refer to the instructional text below.";
+
+        comment(msg);
+    })();
+
+    //aa.env.setValue("CapModel", cap);
+}
+catch (ex) {
+    Avo_LogDebug("ERROR: " + ex.message, 1);
+}
+
+/*------------------------------------------------------------------------------------------------------/
+| END Main Loop
+/------------------------------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------------------------------/
+| BEGIN Functions
+/------------------------------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------------------------------/
+| END Functions
+/------------------------------------------------------------------------------------------------------*/
