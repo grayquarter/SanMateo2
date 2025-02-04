87223cd5fe4868de74e5ba2eb4ed3858421acc6f
GrayQuarter <john@grayquarter.com>
2025-02-04 10:17:47 -0800

    created by Gray Quarter Git Push Tool

---
+++ Scripts/BLD_217_WTUA_SendPermitIssuedEmail.js
@@ -0,0 +1,11 @@
+/*******************************************************
+| Script/Function: sendPermitIssuedEmail() - (ID217)
+| Created by: Jei Yang
+| Created on: 27Jan21
+| Usage: If workflow task 'Ready to Issue Permit' is set to 'Permit Issued', call 54.
+| Modified by: ()
+*********************************************************/
+(function () {
+    //Script 54 Send Permit Issued Email
+    include("BLD_054_ASA_CTRCA_SendPermitIssuedEmail");
+})();
