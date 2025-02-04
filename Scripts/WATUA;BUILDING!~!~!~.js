9b097edafa4685043964ba74c3a74f7eab5cf23d
GrayQuarter <john@grayquarter.com>
2025-02-04 10:20:55 -0800

    created by Gray Quarter Git Push Tool

---
+++ Scripts/WATUA;BUILDING!~!~!~.js
@@ -0,0 +1,9 @@
+//WATUA:Building/*/*/*
+
+if(matches(currentUserID,"AVOCETTE")){
+    showDebug = true;
+    logDebug("In the WATUA event");
+}
+
+// BlueBeam integration - keep at the bottom
+include("BB_WATUA");
