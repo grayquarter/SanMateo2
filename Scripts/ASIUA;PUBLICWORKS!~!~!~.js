42d007d76ff9dcbd33b69aa28f8af90781e83397
GrayQuarter <john@grayquarter.com>
2025-02-04 10:16:11 -0800

    created by Gray Quarter Git Push Tool

---
+++ Scripts/ASIUA;PUBLICWORKS!~!~!~.js
@@ -0,0 +1,13 @@
+//ASIUA:PublicWorks/*/*/*
+
+//Script Set Community Area and Sewer District
+include("PW_ASA_ASIUA_SetAreaAndDistrict");
+
+//Script Send Inspection Results Emails
+include("SMC_ASIUA_SendInspResultsEmails");
+
+
+
+
+// Bluebeam integration - keep at the bottom
+include("BB_ASIUA");
